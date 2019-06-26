8b20b3fa6dc500407029351732dbb9672751a521
Matthew Banbury <mbanbury@gmail.com>
2019-06-26 15:40:54 -0600

    Fixup routes and specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :albums, only: [:index, :show]
+      resources :albums, only: [:index, :show] do
+        resources :songs, only: [:index]
+      end
       resources :artists, only: [:index, :show] do
         resources :albums, only: [:index]
       end
+      resources :songs, only: [:index, :show]
       resources :users, only: [:index, :show, :create]
     end
   end
